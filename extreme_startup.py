96ac88fea71bfd80d60aee13cf38fb3f9b85cd9b
lnordhus <lars.e.nordhus@gmail.com>
2018-09-03 20:34:26 +0200

    added result text

--- extreme_startup.py
+++ extreme_startup.py
@@ -4,26 +4,25 @@ import requests
 from flask import Flask, request
 app = Flask(__name__)
 
+
 def solve_add_question(question):
     return '4'
 
+
 def calculate_answer(question):
-    if '+' in question:
-        return solve_add_question(question)
-    if 'sun' in question:
-        return "Yellow"
     return ''
 
 @app.route("/")
 def answer():
     q = request.args.get("q", "")
+    r = request.args.get("r", "")
     print(q)
+    print(r)
     return calculate_answer(q)
 
 if __name__ == "__main__":
     ip = socket.gethostbyname(socket.gethostname())
-    respons = requests.get("http://150.106.227.80:5000", {'ip':ip})
+    respons = requests.get("http://YOUR IP HERE:5000", {'ip':ip})
 
-    app.run(debug=True, host='150.106.227.80')
-    # app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0')
 
